9f11e26d890f86789bfd702a92e68c925ef12240
최정윤 <jes2ngyun@gmail.com>
2026-07-07 10:19:46 +0900

    init: create Spring Boot project

---
+++ src/main/java/com/sparta/developschedule/DevelopScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.developschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/developschedule/DevelopScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.developschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
